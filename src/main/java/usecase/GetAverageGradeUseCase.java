2db39deb651e6fda6a1ed96deb23e8961b3d9114
Mark Wang <markhywang@gmail.com>
2025-10-06 13:46:18 -0400

    Finished task 3

--- src/main/java/usecase/GetAverageGradeUseCase.java
+++ src/main/java/usecase/GetAverageGradeUseCase.java
@@ -28,6 +28,12 @@ public final class GetAverageGradeUseCase {
         // Call the API to get all the grades for the course for all your team members
         // TODO Task 3a: Complete the logic of calculating the average course grade for
         //              your team members. Hint: the getGrades method might be useful.
+        String[] teamMembers = team.getMembers();
+
+        for (String teamMember : teamMembers) {
+            sum += gradeDataBase.getGrade(teamMember, course).getGrade();
+            count++;
+        }
 
         if (count == 0) {
             return 0;
